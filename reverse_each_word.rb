5eac5a1836339a306391dea1f0ec77d800ca16b6
Olesya Miller <olesyam.miller@gmail.com>
2019-12-11 17:41:39 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,8 @@
 def reverse_each_word(sentence)
   sentence_to_array = sentence.split(/ /)
+  new_array = []
   sentence_to_array.each {
-    |word| return word.reverse()
+    |word| new_array << word.reverse()
   }
+  new_array
 end
