dae2b8f947e76ff9529849ef4ef11d9f9d9fcb48
Olesya Miller <olesyam.miller@gmail.com>
2019-12-11 17:47:17 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -9,7 +9,7 @@
 
 def reverse_each_word(sentence)
   sentence_to_array = sentence.split(/ /)
-  reversed_sentence = sentence_to_array.map {
+  reversed_sentence = sentence_to_array.collect {
     |word| word.reverse()
   }
   reversed_sentence.join(" ")
